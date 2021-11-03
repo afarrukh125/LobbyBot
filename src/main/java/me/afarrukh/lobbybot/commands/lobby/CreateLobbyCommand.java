b68410cf6279c9003222af445047946134b40316
afarrukh125 <33581953+afarrukh125@users.noreply.github.com>
2021-11-03 18:11:50 +0000

    Use optionals without the .get()
    This was a less-than-ideal usage which tested for membership
    and in essence does not end up being much better than passing around
    nulls in terms of code quality (in fact it may be worse due
    to the longer code overall). This has now been improved.
    
    The updates here are still to be tested.

--- src/main/java/me/afarrukh/lobbybot/commands/lobby/CreateLobbyCommand.java
+++ src/main/java/me/afarrukh/lobbybot/commands/lobby/CreateLobbyCommand.java
@@ -7,7 +7,8 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
 
 public class CreateLobbyCommand extends LobbyCommand {
     public CreateLobbyCommand() {
@@ -22,20 +23,29 @@ public class CreateLobbyCommand extends LobbyCommand {
         SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
 
         if (params.isEmpty())
-            lobbyName = evt.getMember().getEffectiveName() + "s-lobby-" + dtf.format(new Date());
+            lobbyName = requireNonNull(evt.getMember()).getEffectiveName() + "s-lobby-" + dtf.format(new Date());
         else
             lobbyName = joinParams(params);
 
-        Optional<Lobby> result = Bot.getInstance().getLobbyManager().createLobby(lobbyName, evt);
-        if (result.isPresent()) {
-            Lobby createdLobby = result.get();
-            evt.getAuthor()
-                    .openPrivateChannel()
-                    .queue(c -> c.sendMessage("Lobby " + createdLobby.getLobbyName() + " was created successfully")
-                            .queue(m -> this.deleteMessage(evt)));
-        } else
-            evt.getAuthor().openPrivateChannel().queue(c -> c.sendMessage("You already have an existing lobby.")
-                    .queue(m -> this.deleteMessage(evt)));
+        Bot.getInstance().getLobbyManager()
+                .createAndReturnLobbyWithName(lobbyName, evt)
+                .ifPresentOrElse(
+                        lobby -> createLobbyAndOpenChannel(evt, lobby),
+                        () -> notifyLobbyAlreadyExists(evt)
+                );
+
+    }
+
+    private void createLobbyAndOpenChannel(GuildMessageReceivedEvent evt, Lobby createdLobby) {
+        evt.getAuthor()
+                .openPrivateChannel()
+                .queue(c -> c.sendMessage("Lobby " + createdLobby.getLobbyName() + " was created successfully")
+                        .queue(m -> this.deleteMessage(evt)));
+    }
+
+    private void notifyLobbyAlreadyExists(GuildMessageReceivedEvent evt) {
+        evt.getAuthor().openPrivateChannel().queue(c -> c.sendMessage("You already have an existing lobby.")
+                .queue(m -> this.deleteMessage(evt)));
     }
 
     @Override
